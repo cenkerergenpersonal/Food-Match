9ecdb4a4f2415ed74bff2318d6a3e8f802466b9e
Cenker <ergencenker@gmail.com>
2025-03-11 21:54:21 +0300

    Add GameInstaller

---
+++ Assets/FoodMatch/Scripts/Installer/GameInstaller.cs
@@ -0,0 +1,9 @@
+using Zenject;
+
+namespace FoodMatch.Scripts.Installer
+{
+    public class GameInstaller : MonoInstaller
+    {
+        //TODO: Container.Bind<Interface>().To(<Class>).AsSignle();
+    }
+}
